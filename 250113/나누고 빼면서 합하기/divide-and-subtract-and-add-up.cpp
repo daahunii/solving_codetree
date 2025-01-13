9a4ec512de98222657dfc9d245f16977bfacc2d6
Dahun Jeong <86994067+daahunii@users.noreply.github.com>
2025-01-13 21:08:40 +0900

    Title: 나누고 빼면서 합하기, Time: 1002ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 250113/나누고 빼면서 합하기/divide-and-subtract-and-add-up.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m, sum = 0;
+int A[100];
+
+
+int main() {
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++) {
+        cin >> A[i];
+    }
+
+    while (true){
+        sum += A[m-1];
+        if (m == 1) break;
+        if (m%2 == 0) m /= 2;
+        else m - 1;
+    }
+
+    cout << sum << endl;
+
+    return 0;
+}
