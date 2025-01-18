3b9d76b8913a53a310d9baa3a993cf2268f9b6c8
Dahun Jeong <86994067+daahunii@users.noreply.github.com>
2025-01-18 18:36:23 +0900

    Title: Factorial, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250118/Factorial/factorial.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+int fact(int n){
+    if(n == 1) return 1;
+    return n * fact(n-1);
+}
+
+int main() {
+    cin >> N;
+    cout << fact(N);
+    return 0;
+}
